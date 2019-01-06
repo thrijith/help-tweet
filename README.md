c18710c4a104a777baf103dea3f26df3d15761be
Thrijith Thankachan <thrijith13@gmail.com>
2019-01-07 00:09:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# help-tweet
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that tweets whenever a label with help is added to an issue.
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how help-tweet could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+## License
+
+[ISC](LICENSE) © 2019 Thrijith Thankachan <thrijith13@gmail.com>
---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports = app => {
+  // Trigger whenever a label is assigned to an issue.
+  app.on('issues.labeled', async context => {
+    // Get Repository Privacy Information.
+    let privacy = context.payload.repository.private
+
+    // Only Tweet for Public Repositories.
+    if (privacy === false) {
+      // Get Labels in the labeled issue.
+      let issueLabels = context.payload.issue.labels
+
+      // Initialize an array with label names.
+      let labels = []
+
+      // Iterate through each label and store it's name.
+      issueLabels.forEach(function (element) {
+        labels.push(element.name)
+      })
+
+      // Check whether an issue contains 'help' or 'help wanted' label.
+      if (labels.includes('help') || labels.includes('help wanted')) {
+        // Get Issue and Repository details for the tweet.
+        let issueURL = context.payload.issue.html_url
+        let repositoryName = context.payload.repository.full_name
+
+        // Initialize Twitter client.
+        let Twitter = require('twit')
+        let t = new Twitter({
+          consumer_key: '...',
+          consumer_secret: '...',
+          access_token: '...',
+          access_token_secret: '...',
+          timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
+        })
+
+        // Form a message for the tweet.
+        let tweet = `
+      📢 We need help with an️ issue in ${repositoryName}. 
+      
+      Issue URL : ${issueURL}
+      
+      Your Help is much appreciated! Thanks 🙂
+      
+      #githubIssueHelp #ghIssueHelp`
+
+        // Post the tweet!
+        t.post('statuses/update', { status: tweet }, function (err, data, response) {
+          if (err) {
+            app.log(response)
+          }
+        })
+      }
+    }
+  })
+}
